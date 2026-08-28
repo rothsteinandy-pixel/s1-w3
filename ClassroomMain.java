f31cf2fecda7f003212e37efd90eacc3ce35464e
Andy <rothstein.andy@gmail.com>
2026-08-28 11:27:13 -0700

    hfh

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,7 +1,7 @@
 public class ClassroomMain {
     public static void main(String[] args) {
-        Classroom c = new Classroom ("davidson", 40 , 33);
-        Classroom c1 = new Classroom("morris");
+        Classroom c = new Classroom ("Davidson", 40 , 33);
+        Classroom c1 = new Classroom("Morris");
         c1.setDesks(30);
        
         c.printState();
